4b06ed10cb2a3acdf812d13778d6002a63b6d1dd
slwu89 <slwu89@berkeley.edu>
2018-08-03 16:17:52 -0700

    fix problem with arrays

--- src/MCR.c
+++ src/MCR.c
@@ -3,7 +3,8 @@
 /* MCR model likelihood */
 SEXP C_logLike_MCRMod(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEXP sB_R,
                       SEXP GFPp_ym_F_R, SEXP GFPp_yp_F_R, SEXP GFPm_ym_F_R, SEXP GFPm_yp_F_R,
-                      SEXP GFPp_ym_M_R, SEXP GFPp_yp_M_R, SEXP GFPm_ym_M_R, SEXP GFPm_yp_M_R)
+                      SEXP GFPp_ym_M_R, SEXP GFPp_yp_M_R, SEXP GFPm_ym_M_R, SEXP GFPm_yp_M_R,
+                      SEXP gens_R)
 {
 
   /* model parameters as C types */
@@ -13,6 +14,8 @@ SEXP C_logLike_MCRMod(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEX
   double sH = asReal(sH_R);
   double sR = asReal(sR_R);
   double sB = asReal(sB_R);
+
+  int gens = asInteger(gens_R);
 
   /* derived parameters */
   double pR = (1.0 - pH)*prR;
@@ -47,31 +50,51 @@ SEXP C_logLike_MCRMod(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEX
   }
 
   /* initial genotype numbers */
-  int HY = 50;
-  int RY = 0;
-  int BY = 0;
-  int WY = 0;
-  int HH = 0;
-  int HR = 0;
-  int HB = 0;
-  int HW = 0;
-  int RR = 0;
-  int RB = 0;
-  int RW = 0;
-  int BB = 0;
-  int BW = 0;
-  int WW = 50;
+  int HY[(gens+1)];
+  HY[0] = 50;
+  int RY[(gens+1)];
+  RY[0] = 0;
+  int BY[(gens+1)];
+  BY[0] = 0;
+  int WY[(gens+1)];
+  WY[0] = 0;
+  int HH[(gens+1)];
+  HH[0] = 0;
+  int HR[(gens+1)];
+  HR[0] = 0;
+  int HB[(gens+1)];
+  HB[0] = 0;
+  int HW[(gens+1)];
+  HW[0] = 0;
+  int RR[(gens+1)];
+  RR[0] = 0;
+  int RB[(gens+1)];
+  RB[0] = 0;
+  int RW[(gens+1)];
+  RW[0] = 0;
+  int BB[(gens+1)];
+  BB[0] = 0;
+  int BW[(gens+1)];
+  BW[0] = 0;
+  int WW[(gens+1)];
+  WW[0] = 50;
 
   /* predicted phenotype numbers */
-  int GFPp_ym_F_Pred = HH + HB + HW;
-  int GFPp_yp_F_Pred = HR;
-  int GFPm_ym_F_Pred = BB;
-  int GFPm_yp_F_Pred = RR + RB + RW + BW + WW;
+  int GFPp_ym_F_Pred = HH[0] + HB[0] + HW[0];
+  int GFPp_yp_F_Pred = HR[0];
+  int GFPm_ym_F_Pred = BB[0];
+  int GFPm_yp_F_Pred = RR[0] + RB[0] + RW[0] + BW[0] + WW[0];
 
-  int GFPp_ym_M_Pred = HY;
+  int GFPp_ym_M_Pred = HY[0];
   int GFPp_yp_M_Pred = 0;
-  int GFPm_ym_M_Pred = BY;
-  int GFPm_yp_M_Pred = RY + WY;
+  int GFPm_ym_M_Pred = BY[0];
+  int GFPm_yp_M_Pred = RY[0] + WY[0];
+
+  /* poulation dynamic model */
+  for(int i=1; i < (gens+1); i++){
+
+  }
+
 
 
   /* loglikelihood of input parameters */
--- src/MCR.h
+++ src/MCR.h
@@ -12,6 +12,7 @@
 /* MCR model likelihood */
 SEXP C_logLike_MCRMod(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEXP sB_R,
                       SEXP GFPp_ym_F_R, SEXP GFPp_yp_F_R, SEXP GFPm_ym_F_R, SEXP GFPm_yp_F_R,
-                      SEXP GFPp_ym_M_R, SEXP GFPp_yp_M_R, SEXP GFPm_ym_M_R, SEXP GFPm_yp_M_R);
+                      SEXP GFPp_ym_M_R, SEXP GFPp_yp_M_R, SEXP GFPm_ym_M_R, SEXP GFPm_yp_M_R,
+                      SEXP gens_R);
 
 #endif

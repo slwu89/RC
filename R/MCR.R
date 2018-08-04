5774fe5c95f9ad146b3fc46f1a10297c2e619238
slwu89 <slwu89@berkeley.edu>
2018-08-03 17:21:48 -0700

    C code done

--- R/MCR.R
+++ R/MCR.R
@@ -7,7 +7,7 @@ logLike_MCRMod <- function(c, pH, prR, sH, sR, sB,
                            GFPp_ym_M, GFPp_yp_M, GFPm_ym_M, GFPm_yp_M,
                            gens){
   .Call(C_logLike_MCRMod,as.double(c),as.double(pH),as.double(prR),as.double(sH),as.double(sR),as.double(sB),
-         as.integer(GFPp_ym_F),as.integer(GFPp_yp_F),as.integer(GFPm_ym_F),as.integer(GFPm_yp_F),
-         as.integer(GFPp_ym_M),as.integer(GFPp_yp_M),as.integer(GFPm_ym_M),as.integer(GFPm_yp_M),
-         as.integer(gens))
+         as.double(GFPp_ym_F),as.double(GFPp_yp_F),as.double(GFPm_ym_F),as.double(GFPm_yp_F),
+         as.double(GFPp_ym_M),as.double(GFPp_yp_M),as.double(GFPm_ym_M),as.double(GFPm_yp_M),
+         as.double(gens))
 }

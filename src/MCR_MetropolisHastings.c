8bbf828e5edf9c2de5f7adff5ffbb136553fd9ec
slwu89 <slwu89@berkeley.edu>
2018-08-04 08:42:10 -0700

    rw metropolis hastings for MCR parameters

--- src/MCR_MetropolisHastings.c
+++ src/MCR_MetropolisHastings.c
@@ -5,6 +5,16 @@
 
 #include "MCR_MetropolisHastings.h"
 #include "MCR.h"
+#include "rv_truncnorm.h"
+
+/* progress bar */
+void printProgress (double percentage){
+  int val = (int) (percentage * 100);
+  int lpad = (int) (percentage * PBWIDTH);
+  int rpad = PBWIDTH - lpad;
+  printf ("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+  fflush (stdout);
+}
 
  /* prior function */
  SEXP C_logPrior(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEXP sB_R){
@@ -54,3 +64,155 @@
                                                   gens_R));
    return ScalarReal(logPrior_val + logLike_val);
  };
+
+
+/* Random Walk Metropolis-Hastings MCMC */
+SEXP C_mcmcMH(SEXP initTheta_R, SEXP proposalSD_R, SEXP numIterations_R,
+              SEXP GFPp_ym_F_1_R, SEXP GFPp_yp_F_1_R, SEXP GFPm_ym_F_1_R, SEXP GFPm_yp_F_1_R,
+              SEXP GFPp_ym_M_1_R, SEXP GFPp_yp_M_1_R, SEXP GFPm_ym_M_1_R, SEXP GFPm_yp_M_1_R,
+              SEXP GFPp_ym_F_2_R, SEXP GFPp_yp_F_2_R, SEXP GFPm_ym_F_2_R, SEXP GFPm_yp_F_2_R,
+              SEXP GFPp_ym_M_2_R, SEXP GFPp_yp_M_2_R, SEXP GFPm_ym_M_2_R, SEXP GFPm_yp_M_2_R,
+              SEXP GFPp_ym_F_3_R, SEXP GFPp_yp_F_3_R, SEXP GFPm_ym_F_3_R, SEXP GFPm_yp_F_3_R,
+              SEXP GFPp_ym_M_3_R, SEXP GFPp_yp_M_3_R, SEXP GFPm_ym_M_3_R, SEXP GFPm_yp_M_3_R,
+              SEXP GFPp_ym_F_4_R, SEXP GFPp_yp_F_4_R, SEXP GFPm_ym_F_4_R, SEXP GFPm_yp_F_4_R,
+              SEXP GFPp_ym_M_4_R, SEXP GFPp_yp_M_4_R, SEXP GFPm_ym_M_4_R, SEXP GFPm_yp_M_4_R,
+              SEXP gens_R){
+
+  /* use R's PRNG */
+  GetRNGstate();
+
+  /* calls to PROTECT */
+  int protectCalls = 0;
+
+  /* Evaluate the log posterior at initTheta: */
+  double logPosteriorThetaCurrent = asReal(C_logPosterior(ScalarReal(REAL_ELT(initTheta_R,0)),
+                                                   ScalarReal(REAL_ELT(initTheta_R,1)),
+                                                   ScalarReal(REAL_ELT(initTheta_R,2)),
+                                                   ScalarReal(REAL_ELT(initTheta_R,3)),
+                                                   ScalarReal(REAL_ELT(initTheta_R,4)),
+                                                   ScalarReal(REAL_ELT(initTheta_R,5)),
+                                                   GFPp_ym_F_1_R, GFPp_yp_F_1_R, GFPm_ym_F_1_R, GFPm_yp_F_1_R,
+                                                   GFPp_ym_M_1_R, GFPp_yp_M_1_R, GFPm_ym_M_1_R, GFPm_yp_M_1_R,
+                                                   GFPp_ym_F_2_R, GFPp_yp_F_2_R, GFPm_ym_F_2_R, GFPm_yp_F_2_R,
+                                                   GFPp_ym_M_2_R, GFPp_yp_M_2_R, GFPm_ym_M_2_R, GFPm_yp_M_2_R,
+                                                   GFPp_ym_F_3_R, GFPp_yp_F_3_R, GFPm_ym_F_3_R, GFPm_yp_F_3_R,
+                                                   GFPp_ym_M_3_R, GFPp_yp_M_3_R, GFPm_ym_M_3_R, GFPm_yp_M_3_R,
+                                                   GFPp_ym_F_4_R, GFPp_yp_F_4_R, GFPm_ym_F_4_R, GFPm_yp_F_4_R,
+                                                   GFPp_ym_M_4_R, GFPp_yp_M_4_R, GFPm_ym_M_4_R, GFPm_yp_M_4_R,
+                                                   gens_R));
+
+  /* number of iterations to run the markov chain */
+  int nIter = asInteger(numIterations_R);
+
+  /* initialise variables */
+  SEXP c_current = PROTECT(ScalarReal(0.0));
+  SEXP pH_current = PROTECT(ScalarReal(0.0));
+  SEXP prR_current = PROTECT(ScalarReal(0.0));
+  SEXP sH_current = PROTECT(ScalarReal(0.0));
+  SEXP sR_current = PROTECT(ScalarReal(0.0));
+  SEXP sB_current = PROTECT(ScalarReal(0.0));
+  protectCalls += 6;
+  SEXP samples = PROTECT(allocMatrix(REALSXP,nIter,6));
+  protectCalls += 1;
+  int accepted = 0;
+
+  /* proposed parameters */
+  SEXP c_proposed = PROTECT(ScalarReal(0.0));
+  SEXP pH_proposed = PROTECT(ScalarReal(0.0));
+  SEXP prR_proposed = PROTECT(ScalarReal(0.0));
+  SEXP sH_proposed = PROTECT(ScalarReal(0.0));
+  SEXP sR_proposed = PROTECT(ScalarReal(0.0));
+  SEXP sB_proposed = PROTECT(ScalarReal(0.0));
+  protectCalls += 6;
+
+  /* parameters of proposal distribution */
+  double c_sd = REAL_ELT(proposalSD_R,0);
+  double pH_sd = REAL_ELT(proposalSD_R,1);
+  double prR_sd = REAL_ELT(proposalSD_R,2);
+  double sH_sd = REAL_ELT(proposalSD_R,3);
+  double sR_sd = REAL_ELT(proposalSD_R,4);
+  double sB_sd = REAL_ELT(proposalSD_R,5);
+
+  /* Run the MCMC algorithm for numIterations interations: */
+  for(int i=0; i < nIter; i++){
+
+    /* draw a new theta from a Gaussian proposal distribution and
+     * assign this to a variable called thetaProposed
+     */
+    c_proposed = ScalarReal(r_truncnorm(0,1,asReal(c_current),c_sd));
+    pH_proposed = ScalarReal(r_truncnorm(0,1,asReal(pH_current),pH_sd));
+    prR_proposed = ScalarReal(r_truncnorm(0,1,asReal(prR_current),prR_sd));
+    sH_proposed = ScalarReal(r_truncnorm(-2,1,asReal(sH_current),sH_sd));
+    sR_proposed = ScalarReal(r_truncnorm(-2,1,asReal(sR_current),sR_sd));
+    sB_proposed = ScalarReal(r_truncnorm(-2,1,asReal(sB_current),sB_sd));
+
+    /* Evaluate the log posterior function at the proposed theta value: */
+    double logPosteriorThetaProposed = asReal(C_logPosterior(c_proposed,pH_proposed,prR_proposed,sH_proposed,sR_proposed,sB_proposed,
+                                                      GFPp_ym_F_1_R, GFPp_yp_F_1_R, GFPm_ym_F_1_R, GFPm_yp_F_1_R,
+                                                      GFPp_ym_M_1_R, GFPp_yp_M_1_R, GFPm_ym_M_1_R, GFPm_yp_M_1_R,
+                                                      GFPp_ym_F_2_R, GFPp_yp_F_2_R, GFPm_ym_F_2_R, GFPm_yp_F_2_R,
+                                                      GFPp_ym_M_2_R, GFPp_yp_M_2_R, GFPm_ym_M_2_R, GFPm_yp_M_2_R,
+                                                      GFPp_ym_F_3_R, GFPp_yp_F_3_R, GFPm_ym_F_3_R, GFPm_yp_F_3_R,
+                                                      GFPp_ym_M_3_R, GFPp_yp_M_3_R, GFPm_ym_M_3_R, GFPm_yp_M_3_R,
+                                                      GFPp_ym_F_4_R, GFPp_yp_F_4_R, GFPm_ym_F_4_R, GFPm_yp_F_4_R,
+                                                      GFPp_ym_M_4_R, GFPp_yp_M_4_R, GFPm_ym_M_4_R, GFPm_yp_M_4_R,
+                                                      gens_R));
+
+    /* Compute the Metropolis-Hastings (log) acceptance probability: */
+    double logAcceptance = logPosteriorThetaProposed - logPosteriorThetaCurrent;
+
+    /* Use a random number to determine if thetaProposed will be accepted: */
+    double randNum = runif(0,1);
+
+    /* If accepted, update the thetaCurrent vector, etc.: */
+    if(randNum < exp(logAcceptance)){
+      /* update parameters */
+      c_current = c_proposed;
+      pH_current = pH_proposed;
+      prR_current = prR_proposed;
+      sH_current = sH_proposed;
+      sR_current = sR_proposed;
+      sB_current = sB_proposed;
+      /* update target log posterior */
+      logPosteriorThetaCurrent = logPosteriorThetaProposed;
+      /* acceptance */
+      accepted += 1;
+    }
+
+    /* Add the current theta to the vector of samples: */
+    SET_REAL_ELT(samples,i+0*nIter,asReal(c_current));
+    SET_REAL_ELT(samples,i+1*nIter,asReal(pH_current));
+    SET_REAL_ELT(samples,i+2*nIter,asReal(prR_current));
+    SET_REAL_ELT(samples,i+3*nIter,asReal(sH_current));
+    SET_REAL_ELT(samples,i+4*nIter,asReal(sR_current));
+    SET_REAL_ELT(samples,i+5*nIter,asReal(sB_current));
+
+    /* progress bar */
+    if(i % 10 == 0){
+      printProgress((double)i/nIter);
+    }
+  }
+
+  /* stop using R's PRNG */
+  PutRNGstate();
+
+  /* prepare output */
+  SEXP names = PROTECT(Rf_allocVector(STRSXP,2));
+  protectCalls += 1;
+  SET_STRING_ELT(names,0,mkChar("samples"));
+  SET_STRING_ELT(names,1,mkChar("acceptance"));
+
+  SEXP out = PROTECT(allocVector(VECSXP, 2));
+  protectCalls += 1;
+  namesgets(out,names);
+
+  SET_VECTOR_ELT(out,0,samples);
+  double acc = (double)accepted/nIter;
+  SET_VECTOR_ELT(out,1,ScalarReal(acc));
+
+  /* unprotect SEXPs for gc */
+  UNPROTECT(protectCalls);
+
+  /* return to R */
+  return out;
+};
--- src/MCR_MetropolisHastings.h
+++ src/MCR_MetropolisHastings.h
@@ -15,6 +15,11 @@
 #include <Rinternals.h>
 #include <Rmath.h>
 
+/* for progress bar */
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+void printProgress(double percentage);
+
 /* prior function */
 SEXP C_logPrior(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEXP sB_R);
 
@@ -29,5 +34,17 @@ SEXP C_logPosterior(SEXP c_R, SEXP pH_R, SEXP prR_R, SEXP sH_R, SEXP sR_R, SEXP 
                   SEXP GFPp_ym_F_4_R, SEXP GFPp_yp_F_4_R, SEXP GFPm_ym_F_4_R, SEXP GFPm_yp_F_4_R,
                   SEXP GFPp_ym_M_4_R, SEXP GFPp_yp_M_4_R, SEXP GFPm_ym_M_4_R, SEXP GFPm_yp_M_4_R,
                   SEXP gens_R);
+
+/* Random Walk Metropolis-Hastings MCMC */
+SEXP C_mcmcMH(SEXP initTheta_R, SEXP proposalSD_R, SEXP numIterations_R,
+              SEXP GFPp_ym_F_1_R, SEXP GFPp_yp_F_1_R, SEXP GFPm_ym_F_1_R, SEXP GFPm_yp_F_1_R,
+              SEXP GFPp_ym_M_1_R, SEXP GFPp_yp_M_1_R, SEXP GFPm_ym_M_1_R, SEXP GFPm_yp_M_1_R,
+              SEXP GFPp_ym_F_2_R, SEXP GFPp_yp_F_2_R, SEXP GFPm_ym_F_2_R, SEXP GFPm_yp_F_2_R,
+              SEXP GFPp_ym_M_2_R, SEXP GFPp_yp_M_2_R, SEXP GFPm_ym_M_2_R, SEXP GFPm_yp_M_2_R,
+              SEXP GFPp_ym_F_3_R, SEXP GFPp_yp_F_3_R, SEXP GFPm_ym_F_3_R, SEXP GFPm_yp_F_3_R,
+              SEXP GFPp_ym_M_3_R, SEXP GFPp_yp_M_3_R, SEXP GFPm_ym_M_3_R, SEXP GFPm_yp_M_3_R,
+              SEXP GFPp_ym_F_4_R, SEXP GFPp_yp_F_4_R, SEXP GFPm_ym_F_4_R, SEXP GFPm_yp_F_4_R,
+              SEXP GFPp_ym_M_4_R, SEXP GFPp_yp_M_4_R, SEXP GFPm_ym_M_4_R, SEXP GFPm_yp_M_4_R,
+              SEXP gens_R);
 
 #endif
